65301edfa1c7e85df9bf0df7afc2bda432aef6b0
robwschlegel <robwschlegel@gmail.com>
2019-05-30 19:41:29 -0300

    Push before getting rid of some testing structure

--- code/functions.R
+++ code/functions.R
@@ -5,10 +5,13 @@
 # Required bits -----------------------------------------------------------
 
 # Packages used in this script
+library(jsonlite, lib.loc = "../R-packages/")
 library(tidyverse) # Base suite of functions
 library(ncdf4) # For opening and working with NetCDF files
 library(lubridate) # For convenient date manipulation
+# unloadNamespace("jsonlite")
 library(heatwaveR, lib.loc = "../R-packages/")
+# cat(paste0("heatwaveR version = ", packageDescription("heatwaveR")$Version))
 
 # Set number of cores
 doMC::registerDoMC(cores = 50)
@@ -19,6 +22,9 @@ options(scipen = 999)
 
 # Corners of the study area
 NWA_corners <- readRDS("data/NWA_corners.Rda")
+
+# Individual regions
+NWA_coords <- readRDS("data/NWA_coords_cabot.Rda")
 
 # The NAPA data location
 NAPA_files <- dir("../../data/NAPA025/1d_grid_T_2D", full.names = T)
@@ -32,18 +38,28 @@ NAPA_coords_sub <- readRDS("data/NAPA_coords_sub.Rda")
 # Load NAPA bathymetry/lon/lat
 NAPA_bathy <- readRDS("data/NAPA_bathy.Rda")
 
+# Load NAPA bathymetry/lon/lat for study area only
+NAPA_bathy_sub <- readRDS("data/NAPA_bathy_sub.Rda")
+
 # Load MHW results
 NAPA_MHW_sub <- readRDS("data/NAPA_MHW_sub.Rda")
 
 # Load NAPA variables
 NAPA_vars <- readRDS("data/NAPA_vars.Rda")
 
+# The base map
+map_base <- ggplot2::fortify(maps::map(fill = TRUE, col = "grey80", plot = FALSE)) %>%
+  dplyr::rename(lon = long) %>%
+  mutate(group = ifelse(lon > 180, group+9999, group),
+         lon = ifelse(lon > 180, lon-360, lon)) %>%
+  select(-region, -subregion)
 
 
 # Extract one variable ----------------------------------------------------
 
 # testers...
 # nc_var <- "sst"
+# nc_var <- "taum"
 # file_name <- NAPA_files[1]
 extract_one_var <- function(file_name, nc_var, coords = NAPA_coords_sub){
   # Open connection
@@ -61,11 +77,23 @@ extract_one_var <- function(file_name, nc_var, coords = NAPA_coords_sub){
     mutate(t = rep(date_seq, each = 388080),
            lat_index = rep(rep(as.numeric(nc$dim$y$vals), each = 528), times = 5)) %>%
     select(lon_index, lat_index, t, var)
-  colnames(want_var)[4] <- nc_var
 
   # Subset pixels to study area
   want_var <- right_join(want_var, coords, by = c("lon_index", "lat_index")) %>%
-    na.omit()
+    na.omit() %>%
+    group_by(lon, lat) %>%
+    filter(mean(var) != 0) %>%
+    # filter(max(t, na.rm = T) == date_end) %>%
+    ungroup()
+  colnames(want_var)[4] <- nc_var
+
+  test <- filter(want_var, t == date_end)
+  ggplot(NAPA_coords, aes(x = lon, y = lat)) +
+    # geom_polygon(data = map_base, aes(group = group), show.legend = F) +
+    geom_point(data = test, aes(colour = taum),
+               shape = 15, size = 0.5) +
+    coord_cartesian(xlim = NWA_corners[1:2],
+                    ylim = NWA_corners[3:4])
 
   # Close connection and exit
   nc_close(nc)
@@ -81,17 +109,36 @@ clim_one_var <- function(one_var, chosen_files = NAPA_files){
   # Load all of the data within the study area for one variable
   system.time(
     all_one_var <- plyr::ldply(chosen_files, extract_one_var, nc_var = one_var, .parallel = T)
-  ) # 201 seconds
+  ) # 200 seconds
 
-  test <- filter(all_one_var, lon_index == 116, lat_index == 340)
-  colnames(test)[4] <- "temp"
-  test <- ts2clm(test, climatologyPeriod = c("1994-01-01", "2015-12-29"), clmOnly = T)
+  # test <- filter(all_one_var, lon_index == 116, lat_index == 340)
+  # colnames(test)[4] <- "temp"
+  # test <- ts2clm(test, climatologyPeriod = c("1994-01-01", "2015-12-29"), clmOnly = T, roundClm = FALSE)
+
+  # test <- all_one_var %>%
+    # group_by(lon, lat) %>%
+    # filter(max(t, na.rm = T) == "2015-12-29") %>%
+    # ungroup()
+    # filter(t == "2015-12-29")
+
+  # ggplot(NWA_coords, aes(x = lon, y = lat)) +
+  #   geom_polygon(data = map_base, aes(group = group), show.legend = F) +
+  #   geom_point(data = test, aes(colour = sst),
+  #              shape = 15, size = 0.5) +
+  #   coord_cartesian(xlim = NWA_corners[1:2],
+  #                   ylim = NWA_corners[3:4])
+
+  # Screen out incomplete time series
+  # system.time(
+  #   all_one_var_sub <- plyr::ddply(all_one_var, c("lon", "lat"), screen_ts, .parallel = T)
+  # ) # 200 seconds
 
   # Calculate climatologies
     # Change the column name to 'temp' for ease of use
   colnames(all_one_var)[4] <- "temp"
   system.time(
-    all_one_clim <- plyr::ddply(all_one_var, c("lon", "lat"), ts2clm, .parallel = T, clmOnly = T,
+    all_one_clim <- plyr::ddply(all_one_var, c("lon", "lat"), ts2clm, .parallel = T,
+                                clmOnly = T,  roundClm = FALSE,
                                 climatologyPeriod = c("1994-01-01", "2015-12-29"))
   ) # xxx seconds
   colnames(all_one_var)[4] <- one_var
