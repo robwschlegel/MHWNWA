44ac335c253cdc971d10cb55aa0d82ec27f52540
robwschlegel <robwschlegel@gmail.com>
2019-06-06 12:59:18 -0300

    Working on inclusion of vectors into SOM pipeline

--- code/functions.R
+++ code/functions.R
@@ -9,9 +9,9 @@ library(jsonlite, lib.loc = "../R-packages/")
 library(tidyverse) # Base suite of functions
 library(ncdf4) # For opening and working with NetCDF files
 library(lubridate) # For convenient date manipulation
-# unloadNamespace("jsonlite")
 library(heatwaveR, lib.loc = "../R-packages/")
 # cat(paste0("heatwaveR version = ", packageDescription("heatwaveR")$Version))
+library(tidync, lib.loc = "../R-packages/")
 
 # Set number of cores
 doMC::registerDoMC(cores = 50)
@@ -128,3 +128,77 @@ clim_one_var <- function(one_var, chosen_files = NAPA_files){
   print(paste0("Finished run on ",one_var," at ",Sys.time()))
 }
 
+
+# Extract one vector ------------------------------------------------------
+
+# testers...
+# nc_vec <- "uoce"
+# file_name <- NAPA_U_files[1]
+extract_one_vec <- function(file_name, nc_vec, coords = NAPA_bathy_sub){
+
+  tidync(file_name) %>% activate(nc_vec)
+  # ncdump::NetCDF(file_name)
+  # Open connection
+  nc <- nc_open(as.character(file_name))
+
+  # Extract dates from file name
+  date_start <- ymd(str_sub(basename(as.character(file_name)), start = 29, end = 36))
+  date_end <- ymd(str_sub(basename(as.character(file_name)), start = 38, end = 45))
+  date_seq <- seq(date_start, date_end, by = "day")
+
+  # Extract and process data
+  want_var <- as.data.frame(ncvar_get(nc, varid = nc_var)) %>%
+    mutate(lon_index = as.numeric(nc$dim$x$vals)) %>%
+    gather(-lon_index, key = lat_index, value = var) %>%
+    mutate(t = rep(date_seq, each = 388080),
+           lat_index = rep(rep(as.numeric(nc$dim$y$vals), each = 528), times = 5)) %>%
+    select(lon_index, lat_index, t, var) %>%
+    right_join(coords, by = c("lon_index", "lat_index")) %>%
+    na.omit()
+  colnames(want_var)[4] <- nc_var
+
+
+  # test <- filter(want_var, t == date_end)
+  # ggplot(NAPA_coords, aes(x = lon, y = lat)) +
+  #   # geom_polygon(data = map_base, aes(group = group), show.legend = F) +
+  #   geom_point(data = test, aes(colour = sst),
+  #              shape = 15, size = 0.5) +
+  #   coord_cartesian(xlim = NWA_corners[1:2],
+  #                   ylim = NWA_corners[3:4])
+
+  # Close connection and exit
+  nc_close(nc)
+  return(want_var)
+}
+
+
+# Calculate clims for one vector ------------------------------------------
+
+# testers...
+# one_vec <- "U"
+clim_one_vec <- function(one_vec, chosen_files = NAPA_files){
+
+  print(paste0("Began run on ",one_var," at ",Sys.time()))
+  # Load all of the data within the study area for one variable
+  # system.time(
+  all_one_var <- plyr::ldply(chosen_files, extract_one_var, nc_var = one_var, .parallel = T)
+  # ) # 200 seconds
+  print(paste0("Finished loading ",one_var," at ",Sys.time()))
+
+  # Calculate climatologies
+  # Change variable column name to 'temp' for ease of use
+  colnames(all_one_var)[4] <- "temp"
+  # system.time(
+  all_one_clim <- plyr::ddply(all_one_var, c("lon", "lat"), ts2clm, .parallel = T,
+                              clmOnly = T,  roundClm = FALSE,
+                              climatologyPeriod = c("1994-01-01", "2015-12-29"))
+  # ) # 230 seconds
+  colnames(all_one_clim)[4] <- one_var
+  all_one_clim$thresh <- NULL
+  print(paste0("Finished calculating ",one_var," clims at ",Sys.time()))
+
+  # Save, clean-up and exit
+  saveRDS(all_one_clim, paste0("data/NAPA_clim_",one_var,".Rda"))
+  rm(all_one_var, all_one_clim); gc()
+  print(paste0("Finished run on ",one_var," at ",Sys.time()))
+}
